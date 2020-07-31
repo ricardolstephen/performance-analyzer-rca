e33b5b706c259df47a7aae81fbb0a0f1f45f1bd6
Ricardo L. Stephen <43506361+ricardolstephen@users.noreply.github.com>
2020-07-31 15:22:37 -0700

    Retain an extra metricsdb file

--- src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/reader/ReaderMetricsProcessor.java
+++ src/main/java/com/amazon/opendistro/elasticsearch/performanceanalyzer/reader/ReaderMetricsProcessor.java
@@ -233,7 +233,7 @@ public class ReaderMetricsProcessor implements Runnable {
       batchMetricsDBSet.clear();
     }
     readBatchMetricsEnabledFromConf();
-    if (batchMetricsDBSet.size() > PluginSettings.instance().getBatchMetricsRetentionPeriod()) {
+    if (batchMetricsDBSet.size() > PluginSettings.instance().getBatchMetricsRetentionPeriod() + 1) {
       Long timestamp = batchMetricsDBSet.pollFirst();
       if (timestamp != null && deleteDBFiles && !metricsDBMap.containsKey(timestamp)) {
         MetricsDB.deleteOnDiskFile(timestamp);
@@ -744,7 +744,7 @@ public class ReaderMetricsProcessor implements Runnable {
   public NavigableSet<Long> getBatchMetrics() {
     if (batchMetricsEnabled) {
       TreeSet<Long> batchMetricsDBSetCopy = new TreeSet<>(batchMetricsDBSet.clone());
-      if (batchMetricsDBSetCopy.size() > PluginSettings.instance().getBatchMetricsRetentionPeriod()) {
+      while (batchMetricsDBSetCopy.size() > PluginSettings.instance().getBatchMetricsRetentionPeriod()) {
         batchMetricsDBSetCopy.pollFirst();
       }
       return Collections.unmodifiableNavigableSet(batchMetricsDBSetCopy);
